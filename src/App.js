81066cf2bc16fe0d876252b5c73e493426225fb6
E2029-Aydin <aydinsanoz2@gmail.com>
2021-12-18 00:01:09 +0300

    title added

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {SafeAreaView, View, Text} from 'react-native';
+import styles from './styles';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View style={styles.container}>
+        <Text>Welcome To Ames tranfer</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
---
+++ src/styles.js
@@ -0,0 +1,19 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    margin: 20,
+    padding: 10,
+    borderColor: 'red',
+    borderRadius: 10,
+    borderWidth: 5,
+  },
+  text: {
+    fontSize: 50,
+  },
+});
+
+export default styles;
